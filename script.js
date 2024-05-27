ab66e559efa92f1f729386680b9ca452591b6dbf
AizaDiusheeva <165205849+AizaDiusheeva@users.noreply.github.com>
2024-05-27 17:07:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let user_name = "Aiza";
+let user_surname ="Diusheeva";
+let year = 2024;
+let number = +prompt ('Enter your born');
+console.log ( 2024-1989);
+let user_age = 35;
+let born = 1989;
+
+
+
+console.log(`Hi, my name is ${user_name} ${user_surname} I was born in ${born}-${year} I'm ${user_age}`);
+
+ 
+let Number = +prompt('Enter the number')
+console.log(Number**3);
